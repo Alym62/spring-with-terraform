e1df11a025483c25555f0ca6dd883dd12ddf8223
Alyasaf Meireles <gasafm62@gmail.com>
2025-01-14 19:44:05 -0300

    feat: structure project.

---
+++ api/src/main/java/com/github/alym62/springterraform/SpringterraformApplication.java
@@ -0,0 +1,13 @@
+package com.github.alym62.springterraform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringterraformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringterraformApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/github/alym62/springterraform/SpringterraformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alym62.springterraform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringterraformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
